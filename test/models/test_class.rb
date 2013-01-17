570b8d02d2e97a235b9e5e487745b3a63c891c88
msalvadores <msalvadores@gmail.com>
2013-01-17 10:53:01 -0800

    Correct setup/teardown in test_class.rb

--- test/models/test_class.rb
+++ test/models/test_class.rb
@@ -3,21 +3,31 @@ require "logger"
 
 class TestClassModel < LinkedData::TestOntologyCommon
 
+  def teardown
+    @os = LinkedData::Models::OntologySubmission.find(@acr + '+' + 1.to_s)
+    unless @os.nil?
+      @os.ontology.load
+      @os.ontology.delete
+      @os.delete
+    end
+  end
+
   def setup
+    @acr = "CSTPROPS"
+    teardown
+    init_test_ontology_msotest @acr
+    @os = LinkedData::Models::OntologySubmission.find(@acr + '+' + 1.to_s)
   end
 
   def test_terms_custom_props
     return if ENV["SKIP_PARSING"]
 
-    acr = "CSTPROPS"
-    init_test_ontology_msotest acr
-    os = LinkedData::Models::OntologySubmission.find(acr + '+' + 1.to_s)
-    os_classes = os.classes
+
+    os_classes = @os.classes
     os_classes.each do |c|
       assert(!c.prefLabel.nil?, "Class #{c.id.value} does not have a label")
     end
-    os.ontology.load
-    os.ontology.delete
-    os.delete
+
   end
+
 end
